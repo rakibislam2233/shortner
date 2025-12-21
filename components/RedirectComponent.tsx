fdfaac6fc9320f1a69bd91f71de33c73820105a8
rakibislam2233 <rakib2020.tkg@gmail.com>
2025-12-22 00:28:55 +0600

    add

--- components/RedirectComponent.tsx
+++ components/RedirectComponent.tsx
@@ -50,36 +50,20 @@ export default function RedirectComponent({
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen lg:p-8 space-y-4">
-      {image ? (
-        <div className="relative w-full lg:max-w-[800px]">
-          {/* Actual image */}
-          <img
-            src={`${API_BASE_URL}/${image}`}
-            alt="Redirecting"
-            width={800}
-            height={600}
-            loading="lazy"
-            onLoad={() => setImageLoaded(true)}
-            className={`lg:max-w-[800px] lg:aspect-[4/3] w-full mx-auto rounded-lg transition-opacity duration-300 ${
-              imageLoaded ? "opacity-100" : "opacity-0"
-            }`}
-          />
-        </div>
-      ) : (
-        // Default skeleton when no image provided
-        <div className="w-full lg:max-w-[800px]">
-          <div className="bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 animate-pulse rounded-lg lg:aspect-[4/3] h-64 lg:h-[600px]">
-            <div className="flex items-center justify-center h-full">
-              <div className="space-y-3 text-center">
-                <div className="w-16 h-16 border-4 border-gray-400 border-t-gray-600 rounded-full animate-spin mx-auto"></div>
-                <p className="text-gray-500 text-lg font-medium">
-                  Redirecting...
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+      <div className="relative w-full lg:max-w-[800px]">
+        {/* Actual image */}
+        <img
+          src={`${API_BASE_URL}/${image}`}
+          alt="Redirecting"
+          width={800}
+          height={600}
+          loading="lazy"
+          onLoad={() => setImageLoaded(true)}
+          className={`lg:max-w-[800px] lg:aspect-[4/3] w-full mx-auto rounded-lg transition-opacity duration-300 ${
+            imageLoaded ? "opacity-100" : "opacity-0"
+          }`}
+        />
+      </div>
     </div>
   );
 }
